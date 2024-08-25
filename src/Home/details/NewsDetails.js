a3e9c885cd913558721c8cd0a46f25a72287aad1
kbbifu <karlbifu.bifu9@gmail.com>
2024-08-25 15:33:12 +0100

    newsdetails

--- src/Home/details/NewsDetails.js
+++ src/Home/details/NewsDetails.js
@@ -30,6 +30,13 @@ function NewsDetails() {
     borderRadius: '10px',
     objectFit: 'cover',
   };
+  const imageStylemobile = {
+    width: '100%',
+    height: '200px',
+    borderRadius: '10px',
+    objectFit: 'cover',
+    marginBottom: '10px',
+  };
 
   return (
     
@@ -47,6 +54,39 @@ function NewsDetails() {
           <div className="dtailsBanner">
             <div className="imageHolder"></div>
             <img src={info.photo} alt="" style={imageStyle} />
+          </div>
+          {info.content?.split('\n').map((paragraph, index) => (
+            <p
+              key={index}
+              className="banner-content"
+              style={{ fontSize: '18px', lineHeight: '1.5' }}
+            >
+              {paragraph}
+            </p>
+          ))}
+          <Link to={-1} style={{ textDecoration: 'none' }}>
+            <button className="backbtn">
+              <span style={{ width: '50px', height: '50px' }}>
+                <img src="/go_back_icon.png" alt="" style={imageStyle} />
+              </span>
+              <span>Retour</span>
+              <span style={{ width: '50px', height: '50px' }}>
+                <img src="/go_back_icon.png" alt="" style={imageStyle} />
+              </span>
+            </button>
+          </Link>
+        </div>
+      </div>  
+
+      <div className='page-container-mobile'>
+        <div className="detailsHeader">
+          <h1 className="headersFont">{info.title}</h1>
+          <p className="banner-content author">
+          <span>Par :</span>  {info.author} <BsDot size={'1rem'} /> <span>{info.publishedDate}</span>
+          </p>
+          <div className="dtailsBanner">
+            <div className="imageHolder"></div>
+            <img src={info.photo} alt="" style={imageStylemobile} />
           </div>
           {info.content?.split('\n').map((paragraph, index) => (
             <p
