671ca73861a58513ec50e22c758f27ddbbffc85c
kbbifu <karlbifu.bifu9@gmail.com>
2024-08-29 05:07:42 +0100

    commit nav

--- src/Home/Nav.js
+++ src/Home/Nav.js
@@ -14,52 +14,51 @@ function Nav() {
 
   const [category, setCategory] = useState([]);
   const navArray = [
-    {id:1,title:'News'},{id:2, title:'Telvision',},{id:3, title:'shows'}]
-  const [navBar, setNavBar] = useState(navArray)
+    { id: 1, title: 'News' },
+    { id: 2, title: 'Telvision' },
+    { id: 3, title: 'shows' },
+  ];
+  const [navBar, setNavBar] = useState(navArray);
   const url = 'http://localhost:5003/category';
-  // useEffect(() => {
-    // console.log(navCat.Navigation)
-    // fetch(url)
-    //   .then((resp) => resp.json())
-    //   .then((resp) => {
-    //     // console.log(resp);
-    //     setCategory(resp);
-    //   });
-  // }, []);
-// useEffect()
-const array  = navCat.Navigation
-useEffect( () => {
-  const findItemById = (arr, itemId) => {
-    return arr.find((item) => item.title === itemId);
-  };
-},[])
+
+  const array = navCat.Navigation;
+  useEffect(() => {
+    const findItemById = (arr, itemId) => {
+      return arr.find((item) => item.title === itemId);
+    };
+  }, []);
 
   const [active, setActive] = useState('nav_men');
   const [toggleIcon, setToggleIcon] = useState('nav_toggler');
-  const [modalActive, setModalActive] = useState("modal");
-  const [modalToggle, setModalToggle] = useState('modalToggle')
+  const [modalActive, setModalActive] = useState('modal');
+  const [modalToggle, setModalToggle] = useState('modalToggle');
 
-  const navToggler =() =>{
-    active === 'nav_men' ? setActive('nav_men nav_active') : setActive('nav_men');
+  const navToggler = () => {
+    active === 'nav_men'
+      ? setActive('nav_men nav_active')
+      : setActive('nav_men');
 
-    // Toggler icon 
+    // Toggler icon
+    toggleIcon === 'nav_toggler'
+      ? setToggleIcon('nav_toggler toggle')
+      : setToggleIcon('nav_toggler');
+  };
 
-    toggleIcon === 'nav_toggler' ? setToggleIcon('nav_toggler toggle') : setToggleIcon('nav_toggler')
-  }
   const navContainer = {
-    width:'100%',
-    boxSizing:'BorderBox',
-  }
-  const logoStyle ={
-    width:'300px',
-    height:'100px',
-    padding:'30px 10px'
-  }
+    width: '100%',
+    boxSizing: 'border-box',
+  };
+
+  const logoStyle = {
+    width: '300px',
+    height: '100px',
+    padding: '30px 10px',
+    marginLeft: '-15px', // Add this line to shift the logo to the left
+  };
 
   return (
     <div className='navi'>
       {/* main navigation */}
-
       <nav className='nav' style={navContainer}>
         <Link to={'/'} className='nav_brand'>
           <img
@@ -71,17 +70,17 @@ useEffect( () => {
         </Link>
         <ul className='nav_menu'>
           <li className='nav_item'>
-              <Link className='nav_link' to={'/'}>
-                Accueil
-              </Link>
+            <Link className='nav_link' to={'/'}>
+              Accueil
+            </Link>
           </li>
-          
+
           <li className='nav_item'>
             <Link className='nav_link' to={'/radio'}>
               Radio
             </Link>
           </li>
-          
+
           <li className='nav_item'>
             <Link className='nav_link' to={'/news'}>
               Actualités
@@ -107,7 +106,6 @@ useEffect( () => {
               Programme
             </Link>
           </li>
-          
         </ul>
         <div className={toggleIcon} onClick={navToggler}>
           <div className='line1'></div>
@@ -119,13 +117,13 @@ useEffect( () => {
         <Link className='nav_link' to={'/'}>
           <li className='nav_item'>Accueil</li>
         </Link>
-        
+
         <li className='nav_item'>
           <Link className='nav_link' to={'/radio'}>
             Radio
           </Link>
         </li>
-        
+
         <Link className='nav_link' to={'/news'}>
           <li className='nav_item'>Actualités</li>
         </Link>
