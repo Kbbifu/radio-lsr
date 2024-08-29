c01909a8a1dfb60a14da2382374b8e3193c96eb7
kbbifu <karlbifu.bifu9@gmail.com>
2024-08-29 01:23:38 +0100

    commit encore

--- src/Home/News.js
+++ src/Home/News.js
@@ -95,15 +95,15 @@ function News() {
           <div className='aLaUne'>
             <h2>A LA UNE</h2>
             <div className='newsGrid'>
-              {aLaUneNews.map((newsCollection) => (
-                <Link to={`/news/${ newsCollection.id}`} key={newsCollection.id} className='newLink'>
+              {aLaUneNews.map((news) => (
+                <Link to={`/news/${ aLaUneNews.id}`} key={aLaUneNews.id} className='newLink'>
                   <div className='newsCard'>
-                    <img src={newsCollection.photo} alt={newsCollection.title} style={imageStyle}/>
+                    <img src={aLaUneNews.photo} alt={aLaUneNews.title} style={imageStyle}/>
                     
                   </div>
                   <div className='newsCardHeader'>
                     <h3 style={headerColor} className='textLimit'>
-                      {newsCollection.title}
+                      {aLaUneNews.title}
                     </h3>
                   </div>
                 </Link>
